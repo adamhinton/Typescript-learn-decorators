4569e99b7ee1c93fd3431c320c02f8a6f00b916d
adamhinton <adam.d.hinton@gmail.com>
2022-05-08 14:20:30 -0400

    make my first decorator

---
+++ src/app.ts
@@ -0,0 +1,12 @@
+// decorators in general are all about classes
+
+class Person {
+  name: "max";
+
+  constructor() {
+    console.log("Creating person object");
+  }
+}
+
+const per = new Person();
+console.log('Per:', per)p
